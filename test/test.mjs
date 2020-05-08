085c8eb5ce778cf4c9198c546cff69864b221303
LambdAurora <aurora42lambda@gmail.com>
2020-05-08 23:34:50 +0200

    :construction: Parser WIP.

--- test/test.mjs
+++ test/test.mjs
@@ -7,13 +7,27 @@
  * see the LICENSE file.
  */
 
-import {md} from "../lib/index.mjs";
+import {md, md_parser} from "../lib/index.mjs";
+
+let link = new md.Link("https://random.com", ["another", "random", "link"], "oh no", "reference_test");
 
 let document = new md.Document()
     .push(new md.Heading("Hello", md.HeadingLevel.H1))
     .push("Sample text")
-    .push(new md.Paragraph([new md.Link("https://random.com", ["another", "random", "link"], "oh no", "reference_test"), new md.Linebreak(), "oh!"]))
-    .push(new md.InlineCode("sample inline code"));
+    .push(new md.Paragraph([link, md.LINEBREAK, "oh!"]))
+    .push(new md.InlineCode("sample inline code"))
+    .push(new md.BlockQuote(["Let's quote some things", link, md.LINEBREAK, new md.InlineCode("some inline code"), "nice?"]));
 
 console.log(JSON.stringify(document, null, 2));
 console.log(document.toString());
+
+console.log(JSON.stringify(md_parser.parse("# lib.md\n\
+\n\
+A Markdown parser and renderer library.\n\
+```js\n\
+let uwu = \"owo\";\n\
+\n\
+console.log(uwu);\n\
+```\
+More text\n\
+"), null, 2));

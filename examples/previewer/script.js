526f1e8063dbe0e31ad00ca4e4826c32e94fd309
LambdAurora <aurora42lambda@gmail.com>
2020-05-19 23:38:54 +0200

    Add inline LaTeX rendering with Katex and some tweaks.

--- examples/previewer/script.js
+++ examples/previewer/script.js
@@ -1,12 +1,17 @@
 import md from "../../lib/index.mjs";
+import katex from 'https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.mjs'; // For inline LaTeX rendering
 
 const markdown_preview = document.getElementById("markdown_preview");
 
 let parser_options = {
+    latex: true,
     newline_as_linebreaks: false
 };
 let render_options = {
     image: { class_name: "responsive_img" },
+    latex: {
+        katex: katex
+    },
     spoiler: { enable: true },
     parent: markdown_preview
 };
@@ -15,6 +20,10 @@ const textarea = document.getElementById("markdown_editor");
 const checkbox_newline_as_linebreaks = document.getElementById("newline_as_linebreaks");
 const checkbox_indent_as_code = document.getElementById("indent_as_code");
 
+if (localStorage.getItem("text")) {
+    textarea.value = localStorage.getItem("text");
+}
+
 textarea.addEventListener("input", render);
 checkbox_newline_as_linebreaks.addEventListener("click", render);
 checkbox_indent_as_code.addEventListener("click", render);
@@ -22,9 +31,17 @@ checkbox_indent_as_code.addEventListener("click", render);
 function render() {
     parser_options.newline_as_linebreaks = checkbox_newline_as_linebreaks.checked;
     parser_options.code_block_from_indent = checkbox_indent_as_code.checked;
-    let doc = md.parser.parse(textarea.value, parser_options);
+
+    localStorage.setItem("text", textarea.value);
+
+    let start = new Date().getTime();
+    let markdown_doc = md.parser.parse(textarea.value, parser_options);
+    console.log("Parsed in: " + (new Date().getTime() - start) + "ms");
     markdown_preview.innerHTML = "";
-    md.render(doc, document, render_options);
+    start = new Date().getTime();
+    md.render(markdown_doc, document, render_options);
+
+    console.log("Rendered in: " + (new Date().getTime() - start) + "ms");
 
     document.querySelectorAll(".spoiler_hidden").forEach(spoiler => {
         spoiler.addEventListener("click", _ => {
